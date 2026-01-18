03d192f8f266299e4cce042690864c77f6f3b113
maria kuzmenko <mariaakuzmenko@gmail.com>
2026-01-18 03:28:41 -0500

    fixed mobile responsiveness

--- src/pages/navbar.jsx
+++ src/pages/navbar.jsx
@@ -5,11 +5,7 @@ function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleMenu = () => {
-    if (isOpen) {
-      setIsOpen(false);
-    } else {
-      setIsOpen(true);
-    }
+    setIsOpen((prev) => !prev);
   };
 
   return (
@@ -18,7 +14,7 @@ function Navbar() {
         <div className="nav-left">
           <img src={flower} alt="Maria logo" className="nav-logo" />
         </div>
-        <ul className='nav-list'>
+        <ul className={`nav-list ${isOpen ? "is-open" : ""}`} id="primary-nav">
           <li className='list-item'>
             <a href="/" className='link-item'>home</a>
           </li>
@@ -27,6 +23,16 @@ function Navbar() {
           </li>
           <li className='list-item'>
             <a href="/#experience-section" className='link-item'>experience</a>
+          </li>
+          <li className='list-item'>
+            <a
+              href="/pdfs/MariaKuzmenko_Resume.pdf"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="link-item"
+            >
+              resume
+            </a>
           </li>
           {/* <li className='list-item has-dropdown'>
                 <button type='button' onClick={toggleMenu} className="link-item">options</button>
@@ -44,21 +50,18 @@ function Navbar() {
               </li> */}
         </ul>
         <div className="nav-right">
-          <a
-            href="/pdfs/MariaKuzmenko_Resume.pdf"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="link-item resume-link"
-            aria-label="Resume"
+          <button
+            type="button"
+            className="nav-toggle"
+            aria-label="Toggle navigation"
+            aria-controls="primary-nav"
+            aria-expanded={isOpen}
+            onClick={toggleMenu}
           >
-            <span className="resume-text">resume</span>
-            <span className="resume-icon" aria-hidden="true">
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-text" viewBox="0 0 16 16">
-                <path d="M5 4a.5.5 0 0 0 0 1h6a.5.5 0 0 0 0-1zm-.5 2.5A.5.5 0 0 1 5 6h6a.5.5 0 0 1 0 1H5a.5.5 0 0 1-.5-.5M5 8a.5.5 0 0 0 0 1h6a.5.5 0 0 0 0-1zm0 2a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1z" />
-                <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2zm10-1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1" />
-              </svg>
-            </span>
-          </a>
+            <span className="nav-toggle-bar" />
+            <span className="nav-toggle-bar" />
+            <span className="nav-toggle-bar" />
+          </button>
         </div>
       </div>
     </nav>
